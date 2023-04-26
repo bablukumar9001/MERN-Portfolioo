75c9a3641f9876745c9c48cd056cb30b82c50fd2
Bablu <bk559722@gmail.com>
2023-04-26 16:13:49 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+require('dotenv').config()
+require("./db/conn")
+const clientRouter = require("./routers/clientData")
+
+const app = express()
+const PORT = process.env.PORT || 7000
+app.use(express.json())
+app.use(clientRouter)
+
+
+
+
+app.listen(PORT, () => {
+    console.group(`this app listning on  port no. ${PORT}`)
+})
+
